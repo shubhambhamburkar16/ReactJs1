1e4d4aa0e16defa558165d860b205d585ea7446f
shubhambhamburkar16 <144081543+shubhambhamburkar16@users.noreply.github.com>
2024-01-01 15:37:09 -1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+import CodeMind from './tutorial/CodemindWebsite';
+function App() {
+  return (
+    <div className='App'>
+      <CodeMind/>
+    </div>
+
+  );
+}
+
+export default App;
