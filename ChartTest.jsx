a1086d5aafe193ceb77a25d6356a875c327f80ac
shubhambhamburkar16 <144081543+shubhambhamburkar16@users.noreply.github.com>
2024-01-15 17:30:51 -1200

    Add files via upload

---
+++ ChartTest.jsx
@@ -0,0 +1,72 @@
+// import React, { Component } from "react";
+// import Chart from "react-apexcharts";
+
+// class ChartsTest extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       options: {
+//         chart: {
+//           id: "basic-bar"
+//         },
+//         xaxis: {
+//           categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+//         }
+//       },
+//       series: [
+//         {
+//           name: "series-1",
+//           data: [30, 40, 45, 50, 49, 60, 70, 91]
+//         }
+//       ]
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div className="app">
+//         <div className="row">
+//           <div className="mixed-chart">
+//             <Chart
+//               options={this.state.options}
+//               series={this.state.series}
+//               type="line"
+//               width="500"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ChartsTest;
+
+
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts'
+
+class ChartsTest extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {},
+      series: [44, 55, 41, 17, 15],
+      labels: ['A', 'B', 'C', 'D', 'E']
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="donut">
+        <Chart options={this.state.options} series={this.state.series} type="donut" width="380" />
+      </div>
+    );
+  }
+}
+
+export default ChartsTest;
